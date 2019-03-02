d320060d2f73b16da472341140c216c09a3244f0
griffin-stewie <panterathefamilyguy@gmail.com>
2019-03-02 15:43:59 +0900

    workaround for building failure via fastlane

--- Build-Phases/install_using_xcode.sh
+++ Build-Phases/install_using_xcode.sh
@@ -1,7 +1,15 @@
 #!/bin/bash
 
+if [ -n "$BUILD_FROM_FASTLANE" ]; then
+    echo "Build from fastlane. skip execute Install plugin phase."
+    exit 0
+fi
+
 ## This script is for using inside Xcode's Run Script Phase to package framework as .sketchplugin and putitng it as symbolic link.
 ## It means that all you have to do to debug using Xcode is just "Run" then automatilcally install my sketchplugin.
+
+export LC_ALL=en_US.UTF-8
+export LANG=en_US.UTF-8
 
 ## set a path for `bundler` if you have it on different path.
 BUNDLER_PATH=~/.rbenv/shims/bundler
